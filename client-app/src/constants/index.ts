3b2dad5e321e6b712a99b0e6c4c2f74c4507ff9f
Stefan Krstic <stefankrstic96@yahoo.com>
2021-06-27 11:07:13 +0200

    feat(): add admin panel

--- client-app/src/constants/index.ts
+++ client-app/src/constants/index.ts
@@ -1,3 +1,6 @@
+//Note: using in all paginations, should change
+export const PAGE_LIMIT = 1;
+
 export const roles = [
   { key: "co", text: "Consultant", value: "consultant" },
   { key: "cl", text: "Client", value: "client" },
