8dfc5a9733831bd35ff1cfbfc4d303c7e6073ca5
Stefan <stefantosic.dev@gmail.com>
2022-01-18 14:58:00 +0100

    Create package validation

--- Application/Packages/Create.cs
+++ Application/Packages/Create.cs
@@ -2,6 +2,8 @@
 using Application.DTOs;
 using Application.Interfaces.Repositories.Package;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,15 +17,22 @@ namespace Application.Packages
 		}
 		public class Handler : IRequestHandler<Command, Result<Unit>>
 		{
+			private readonly DataContext _context;
 			private readonly IPackageRepository _packageRepository;
 
-			public Handler(IPackageRepository packageRepository)
+			public Handler(DataContext context, IPackageRepository packageRepository)
 			{
+				_context = context;
 				_packageRepository = packageRepository;
 			}
 
 			public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
 			{
+				if (await _context.Packages
+					.AnyAsync(x => x.MentorId == request.Package.MentorId))
+				{
+					return Result<Unit>.Failure("Već ste napravili paket");
+				}
 				if (await _packageRepository.AddAsync(request.Package))
 				{
 					return Result<Unit>.Success(Unit.Value);
