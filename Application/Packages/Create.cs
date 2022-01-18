2688fdcd6ee471d607eb376b7e862b6114265406
Stefan <stefantosic.dev@gmail.com>
2022-01-18 14:44:53 +0100

    Create package

---
+++ Application/Packages/Create.cs
@@ -0,0 +1,35 @@
+﻿using Application.Core;
+using Application.DTOs;
+using Application.Interfaces.Repositories.Package;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Packages
+{
+	public class Create
+	{
+		public class Command : IRequest<Result<Unit>>
+		{
+			public PackageDto Package { get; set; }
+		}
+		public class Handler : IRequestHandler<Command, Result<Unit>>
+		{
+			private readonly IPackageRepository _packageRepository;
+
+			public Handler(IPackageRepository packageRepository)
+			{
+				_packageRepository = packageRepository;
+			}
+
+			public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+			{
+				if (await _packageRepository.AddAsync(request.Package))
+				{
+					return Result<Unit>.Success(Unit.Value);
+				}
+				return Result<Unit>.Failure("Nismo uspeli da dodamo novi paket usluge");
+			}
+		}
+	}
+}
